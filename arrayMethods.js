eb5cb4ef1a337a1e02842ac074d4e134726abf4e
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-06-06 22:08:11 +0100

    f

--- arrayMethods.js
+++ arrayMethods.js
@@ -3,12 +3,19 @@ let names = ["jude","joseph","peter","silas"];
 names.forEach(function (user) {
     console.log(user)
 })
-//map function: we want to map all elements to a new array and set all items to uppercase
+//MAP function: we want to map all elements to a new array and set all items to uppercase
 let newNames = names.map(function (n) {
     return n.toUpperCase();
 })
 console.log(newNames)
-//include function: the function below checks if a specific character exist in an array.
+// returns an array of all the names where every name is in lower case.
+function lowerCaseNames(allNames) {
+    return allNames.map(function (n) {
+        return n.toLowerCase()
+    })
+}
+console.log(lowerCaseNames(names))
+//INCLUDE function: the function below checks if a specific character exist in an array.
 let characters = ["A","G","P","T"];
 function hasCharacter(character,char) {
     if(character.includes(char))
@@ -17,3 +24,10 @@ function hasCharacter(character,char) {
         return false;
 }
 console.log(hasCharacter(characters,"A"))
+//join function: the function exportCSV returns a string containing the names separated by a comma.
+function exportCSV(allNames){
+    let aN = allNames.join(",");
+    return aN;
+}
+console.log(exportCSV(names))
+
