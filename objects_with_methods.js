c9f0771a65c27ccb3197e9030534ceec06607659
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-06-07 11:20:32 +0100

    objects with methods

--- objects_with_methods.js
+++ objects_with_methods.js
@@ -1,10 +1,23 @@
 //a function can be a property of an object and called accordingly
 let person = {
-    firstName:"john",
-    lastName:"doe",
+    firstName:"Ashu",
+    lastName:"Nyenti",
     age:19,
     canVote:function (g) {
         return g>=18;
+    },
+    getFullName:function () {
+      return `${this.firstName} ${this.lastName}`;
+    },
+    marks:[10,80,20],
+    getSumOfMarks:function () {
+        let k = 0;
+        this.marks.forEach(function (s) {
+            k = k + s;
+        });
+        return k;
     }
 }
 console.log(person.canVote(person.age))
+console.log(person.getFullName())
+console.log(person.getSumOfMarks())
