98bae7d5202f276694e04dac7b67130baa2f1dd3
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-06-07 22:11:51 +0100

    classes in javascript

---
+++ classes/class_constructors.js
@@ -0,0 +1,7 @@
+//recall that a constructor is a special method that is called automatically when a class is instantiated.
+class User{
+    constructor(){
+        console.log("this is a constructor");
+    }
+}
+let user = new User();
---
+++ classes/index.js
@@ -0,0 +1,3 @@
+import User from "./User.js";
+let user = new User();
+console.log(user)
