a47a9653613232ae346eaadaf0cb87c99e724d87
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-06-11 13:59:48 +0100

    Overall billing

--- classes/overall_project_billing_app.js
+++ classes/overall_project_billing_app.js
@@ -32,6 +32,7 @@ const bill = new Bill();
 bill.addAmount(500);
 bill.addAmount(4500);
 bill.addAmount(1500);
+bill.addAmount(4500);
 console.log(bill.getTotal())
 console.log(bill.getCount())
 console.log(bill.getAverage())
