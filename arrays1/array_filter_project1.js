8a62568de96a5c5f622b33fdb7df207aa64576fe
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-06-17 11:24:35 +0100

    b

--- arrays1/array_filter_project1.js
+++ arrays1/array_filter_project1.js
@@ -1,7 +1,7 @@
 /*
 Complete the function getPositiveTemperatures such that it returns an array containing the positive temperatures (the temperatures that are above 0).
  */
-const temps = [8,35,-10,4,-5,-1,48];
+let temps = [8,35,-10,4,-5,-1,48];
 function getPositiveTemperatures(t) {
     return t.filter(function (temperature) {
         return temperature > 0;
