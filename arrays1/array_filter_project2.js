f3227483ed234aba92bf1a459f608fd7f98e2c11
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-06-17 11:55:52 +0100

    filter projects2

--- arrays1/array_filter_project2.js
+++ arrays1/array_filter_project2.js
@@ -9,3 +9,27 @@ function fiveCharactersOrFewerOnly(str) {
     });
 }
 console.log(fiveCharactersOrFewerOnly(words));
+
+/*
+Given an array of people objects, compelte the function peopleWhoBelongToAClub return a new array that has filtered out
+ all those who don't belong to the club.
+ */
+const people = [
+    { name: "Angelina Jolie", member: true },
+    { name: "Eric Jones", member: false },
+    { name: "Paris Hilton", member: true },
+    { name: "Kayne West", member: false },
+    { name: "Bob Ziroll", member: true }
+];
+function peopleWhoBelongToAClub(club) {
+    return club.filter((belong)=>{
+        return belong.member===true;
+    });
+};
+function peopleWhoBelongToAClub2(club) {
+    return club.filter(function(belong){
+        return belong.member===true;
+    });
+};
+//both functions will return the same thing, but the first is using THE ARROW OPERATOR. both are using callbacks.
+console.log(peopleWhoBelongToAClub2(people));
