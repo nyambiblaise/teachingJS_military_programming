1b1e0182a2946ce5e5e87eb0088d36ccbe96e78f
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-06-25 18:14:13 +0100

    String II functions

---
+++ 11_strings2/spilt_function.js
@@ -0,0 +1,6 @@
+/*
+The .split(separator) method divides the string into an array of substrings based on the separator you provide.
+ */
+let apps = "Calculator,Phone,Contacts";
+let appsArray = apps.split(","); //['Calculator','Phone','Contacts']
+console.log(appsArray)
