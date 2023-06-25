4dc9e18c7ac98743f7b6c29db158ca00636edd9f
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-06-25 18:16:16 +0100

    String II functions

--- 11_strings2/spilt_function.js
+++ 11_strings2/spilt_function.js
@@ -1,6 +1,13 @@
 /*
 The .split(separator) method divides the string into an array of substrings based on the separator you provide.
+
+This is especially useful to convert from CSV (Comma Separated Values) back to an array.
+Reminder that the opposite of String.split(separator) would be Array.join(glue).
  */
 let apps = "Calculator,Phone,Contacts";
 let appsArray = apps.split(","); //['Calculator','Phone','Contacts']
-console.log(appsArray)
+console.log(appsArray);
+
+let message = `hello guys, how do you do? hope all good?`;
+let messageArray = message.split(" ");
+console.log(messageArray);
