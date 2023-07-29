438beef1fe3a86b492e36ddf7a3bc26e5d569da2
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-07-28 23:02:14 -0400

    Arrays3

--- 12_arrays3/array_some_method.js
+++ 12_arrays3/array_some_method.js
@@ -5,3 +5,13 @@ const below10 = numbers.some(n=> n<10)//true
 
 console.log(above10);
 console.log(below10);
+
+//Complete the function shouldAdjustGrades such that it returns true when the grades need to be increased.
+// Grades should be increased when at least one of them is failing (10 and above are passing).
+const grades1 = [10, 12, 10, 14];
+const grades2 = [12, 8, 17];
+
+const shouldAdjustGrades = grades => {
+    return grades.some(g => g<10);
+}
+console.log(shouldAdjustGrades(grades2));
