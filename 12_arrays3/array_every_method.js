72700524766fca973dc9cc66cbbf2e558d12f53e
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-07-28 23:08:27 -0400

    Arrays3

--- 12_arrays3/array_every_method.js
+++ 12_arrays3/array_every_method.js
@@ -5,3 +5,10 @@ let nbBelow13 = numbers.every(number=>number<13); //true
 
 console.log(nbAbove10);
 console.log(nbBelow13);
+
+//Complete the function shouldCancelExam such that it returns true when the exam needs to be canceled.
+// An exam must be canceled if all the students' grades were very high (18 and above).
+const shouldCancelExam = grades => {
+    return grades.every(grade=>grade>=18);
+}
+console.log(`Should Exams be cancelled?: ${shouldCancelExam(grades1)}`);
