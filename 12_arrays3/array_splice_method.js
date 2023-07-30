a02d91dcb51c082e2cfa06e99fcba6ec7398f64e
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-07-30 13:33:43 -0400

    n

--- 12_arrays3/array_splice_method.js
+++ 12_arrays3/array_splice_method.js
@@ -1,7 +1,10 @@
 //You can also delete specific items from an array using the splice(start[, deleteCount]) method.
 const names =["john","mary","judas","peter","silas"];
 
-//displaying all the members
-let allNames = names=>{
-    return names.reduce();
-}
+console.log("BeFore Deleting");
+console.log(names);
+
+names.splice(1,2);
+
+console.log("After Deleting");
+console.log(names);
