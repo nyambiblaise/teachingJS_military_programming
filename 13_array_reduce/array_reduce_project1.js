0ce9d840092ce05defebc196e4b35a61251f4e57
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-07-30 14:43:07 -0400

    array reduce method

--- 13_array_reduce/array_reduce_project1.js
+++ 13_array_reduce/array_reduce_project1.js
@@ -1,9 +1,15 @@
 /*
-There are no tasks and no tests in this challenge. All you have to do is visualize in the console the value of total, current, and finally the sum. Notice how:
-
+For the array of grades, display the current value and total after each iteration. At the end, display the sum of the array elements
 the value of total starts at 0 (which is given by the initialValue)
 the value of current a single item of the array for every iteration.
 the value of total is the result of the last run of the reducer (the ongoing sum)
  */
 
 let grades = [10, 5, 15, 20];
+let sumOfGrades = grades.reduce((total,current)=>{
+    console.log(`Current Value: ${current}`);
+    console.log(`Total: ${total}`);
+    console.log(`=========`);
+    return total + current;
+},0);
+console.log(sumOfGrades);
