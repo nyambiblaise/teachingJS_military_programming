cf8065213c064778696750212a6c03afb7694cf0
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-07-30 15:32:36 -0400

    array contactenation method

---
+++ 14_array_destructing_contact/array_destruct_exo1.js
@@ -0,0 +1,16 @@
+//Rewrite the code below to use array destructuring instead of assigning each value to a variable.
+console.log("EXERCISE 1");
+let item = ["Egg", 0.25, 12];
+let name = item[0];
+let price = item[1];
+let quantity = item[2];
+console.log(`Item: ${name}, Quantity: ${quantity}, Price: ${price}`);
+
+//solving it using the destruct method...
+let newItem = ["Egg",0.25,12];
+let showItems = someItems=>{
+    const [name,qty,price] = someItems;
+    return `Item: ${name}, Quantity: ${qty}, Price: ${price}`
+}
+console.log(showItems(newItem));
+
