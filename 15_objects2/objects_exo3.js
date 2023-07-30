5ce66f70e5d70f29eeca9b635fb6667b8a3a57b9
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-07-30 18:22:52 -0400

    array objects II

--- 15_objects2/objects_exo3.js
+++ 15_objects2/objects_exo3.js
@@ -3,3 +3,12 @@ Complete the function getCountProperties such that it returns the number of keys
 
 console.log(getCountProperties({id: 1, name: "Learn JavaScript", year: 2021, category: "Programming"})); // 4
  */
+let courses = {
+    id:1,
+    name:"Learn JavaScript",
+    year:2023
+};
+let getCountProperties = someCourses =>{
+    return Object.keys(someCourses).length;
+}
+console.log(getCountProperties(courses))
