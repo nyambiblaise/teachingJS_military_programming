80d056dab1e6ac24b33a9782bd355a3176333b30
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-07-30 23:48:23 -0400

    object destructing and channing

--- 17_optional_channing/chainning_with_arrays.js
+++ 17_optional_channing/chainning_with_arrays.js
@@ -8,3 +8,21 @@ if (data.temperatures) {
 }
 //We can refactor it into:
 const firstValue = data.temperatures?.[0];
+/*
+Notice how we used ?. in front of the [0] to access the first item of the array. The benefit of optional chaining here is that if data.temperatures returned null or undefined, your code won't break. It will short-circuit and return undefined. Which is why we were able to get rid of the if condition.
+
+Optional chaining usage with functions
+Similarly, we can use optional chaining to call functions by using the ?. operator. Here's the example before optional chaining:
+ */
+const person = {
+    age: 43,
+    name: "Sam"
+};
+
+let upperCasedName = person.name; // might be undefined
+if (person.name) {
+    upperCasedName = person.name.toUpperCase();
+}
+//Which we can refactor into:
+const upperCasedName = person.name?.toUpperCase();
+//This will only call the .toUpperCase() method if person.name does not evaluate to null or undefined.
