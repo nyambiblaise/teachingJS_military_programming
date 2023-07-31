4e288bb3d947ba94da34ec00205e9f171eef3c9c
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-07-30 23:05:55 -0400

    n

--- 17_optional_channing/introduction.js
+++ 17_optional_channing/introduction.js
@@ -1,4 +1,16 @@
 /*
 We know from a previous lessons that if an object property returns undefined, then we should avoid accessing other
  properties on it (or calling methods on it) so that we don't get an error.
+Optional chaining  allows you to access a property deep within an object without risking an error if one of the
+ properties is null or undefined.
  */
+const user = {
+    details: {
+        name: {
+            firstName: "Sam"
+        }
+    },
+    data: null
+}
+const uName = user.details.name.firstName; //same
+console.log(uName);
