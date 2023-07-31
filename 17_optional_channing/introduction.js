fb20e7cf704106cc6c3df69b7046b002dbb55a0c
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-07-30 23:10:09 -0400

    n

--- 17_optional_channing/introduction.js
+++ 17_optional_channing/introduction.js
@@ -3,6 +3,9 @@ We know from a previous lessons that if an object property returns undefined, th
  properties on it (or calling methods on it) so that we don't get an error.
 Optional chaining  allows you to access a property deep within an object without risking an error if one of the
  properties is null or undefined.
+
+ NOTE:
+ One important note is that you cannot use optional chaining on an object that may not exist. The object has to exist. Optional chaining is only used to access a property that may or may not exist.
  */
 const user = {
     details: {
@@ -12,6 +15,6 @@ const user = {
     },
     data: null
 }
-const fName = user.details.name.firstName; //sam
-const lName = user.details.name?.lastName; //undefined
+const fName = user.details?.name?.firstName; //sam
+const lName = user.details?.name?.lastName; //undefined
 console.log(lName);
