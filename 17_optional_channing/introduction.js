55b800eae07d240c1a1ecf6473bf1f4a4e3d6821
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-07-30 23:16:47 -0400

    array objects III

--- 17_optional_channing/introduction.js
+++ 17_optional_channing/introduction.js
@@ -6,6 +6,9 @@ Optional chaining  allows you to access a property deep within an object without
 
  NOTE:
  One important note is that you cannot use optional chaining on an object that may not exist. The object has to exist. Optional chaining is only used to access a property that may or may not exist.
+
+ You may have noticed that we've been writing user.details?.name instead of user?.details?.name that's because we know for sure that user is an object. So we don't necessarily need optional chaining for user.details.
+In case you're not sure whether user is an object, then you can write user?.details?.name. However, you still have to make sure that there is a variable user defined, or else you will get an error.
  */
 const user = {
     details: {
