6a00c6e8f2ddb5725a76f5f1676de8e427b410ea
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-07-31 00:02:29 -0400

    object destructing and channing

--- 17_optional_channing/warning_against_optional_chainning.js
+++ 17_optional_channing/warning_against_optional_chainning.js
@@ -18,4 +18,10 @@ Recap
 Optional chaining can be used for arrays. The syntax is ?.[index]
 Optional chaining can be used for functions. The syntax is functionName?.()
 Optional chaining cannot be used for assignment. It's only used for reading.
+Optional chaining allows you to access a property deep within an object without risking an error if one of the properties is null or undefined.
+In case one of the properties is null or undefined, then the ?. will short-circuit to undefined.
+You cannot use optional chaining on an object that may not exist. The object has to exist. Optional chaining is only used to access a property that may or may not exist.
+Optional chaining can be used for arrays. The syntax is ?.[index]
+Optional chaining can be used for functions. The syntax is functionName?.()
+Optional chaining cannot be used for assignment. It's only used for reading.
  */
