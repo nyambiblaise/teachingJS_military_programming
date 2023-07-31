5b20e3fd747fe3672f0c030c002e38c4ffb26b12
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-07-30 22:48:36 -0400

    array objects III

---
+++ 16_objects3/destructing_exo2.js
@@ -0,0 +1,12 @@
+//Complete the function getLatLng such that it destructures the userLocation object into 2 variables: lat and lng.
+// After destructuring successfully, the function will automatically return a string that describes the location.
+
+const userLocation = {
+    lat: 24.235235,
+    lng: 2.5734,
+};
+let getLatLng = someLocation=>{
+    const [lat,lng] = someLocation;
+    return `Longitude is ${lng} and Latitude is ${lat}`;
+}
+console.log(getLatLng(userLocation));
