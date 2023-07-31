2448e367a01be02aff6ba961f754d3d5b884df8f
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-07-30 23:03:40 -0400

    n

---
+++ 17_optional_channing/introduction.js
@@ -0,0 +1,4 @@
+/*
+We know from a previous lessons that if an object property returns undefined, then we should avoid accessing other
+ properties on it (or calling methods on it) so that we don't get an error.
+ */
