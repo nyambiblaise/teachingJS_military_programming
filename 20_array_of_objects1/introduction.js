1cf36781e623f91b8f2083038d5863b5ce2046e3
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-07-31 21:31:33 -0400

    arrays of objects 1

---
+++ 20_array_of_objects1/introduction.js
@@ -0,0 +1,5 @@
+/*
+Arrays of objects are the most common data type that you will encounter when working in web development, that's because most APIs (for example, a weather API, Twitter API, etc.) return arrays of objects.
+
+From a technical point of view, arrays of objects don't have special functionality or additional methods.
+ */
