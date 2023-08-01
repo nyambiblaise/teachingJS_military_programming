38e7eaf88332967ae6a5de9377723b9fd84c6d90
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-07-31 20:25:50 -0400

    advanced control flow

--- 19_advanced_control_flow/introduction.js
+++ 19_advanced_control_flow/introduction.js
@@ -3,7 +3,7 @@ we're going to touch on a common scenario where you can completely get rid of if
 
 Let's say you're building an app like Deliveroo and you have a method that sends push notifications to your users updating them on the progress of their order:
  */
-const getPushMessage = status => {
+const getPushMessage1 = status => {
     if (status === "received") {
         return "Restaurant started working on your order.";
     } else if (status === "prepared") {
@@ -16,3 +16,14 @@ const getPushMessage = status => {
         return "Unknown status";
     }
 }
+//we can simplify this by creating an object with all the various statuses
+const getPushMessage = status=>{
+    const message = {
+        received:"Restaurant started working on your order.",
+        prepared:"Driver is picking up your food.",
+        enroute:"Driver is cycling your way!",
+        arrived:"Enjoy your food!",
+    };
+    return message[status]??"Unknown";
+}
+//Notice how we also used nullish coalescing to handle the else case when the status is different than the ones we provided.
