ead198ba782a9c302749d40553ebd7ddf79e3755
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-07-31 20:08:56 -0400

    nullish coalsceing

---
+++ 18_nullish_coalescing/nullish_exo4.js
@@ -0,0 +1,9 @@
+/*
+Complete the function getFullName such that it returns the full name in lower case from the user object when it exists. Otherwise, it should return "user".
+ */
+const user = {
+    info:{
+        name:"john",
+        age:19
+    }
+};
