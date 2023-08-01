3290802dff6597a9cc1ad543dc510094a517dfde
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-07-31 22:02:28 -0400

    arrays of objects 1

---
+++ 20_array_of_objects1/arrayOfObjects_exo3.js
@@ -0,0 +1,18 @@
+/*
+Complete the getSumOfGrades function such that it returns the sum of all grades.
+This should work for any array received as a parameter. The array can have 1 or more grade entries in it.
+ */
+const grades = [
+    {
+        date:"2023-07-31 18:18",
+        grade:14
+    },
+    {
+        date:"2023-08-01 10:21",
+        grade:10
+    }
+]
+const getSumofGrades = someGrades=>{
+
+}
+console.log(getSumofGrades(grades));
