c7f9665310f137b6388553383b14e0f1de7012c8
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-07-31 22:54:07 -0400

    arrays of objects 1

--- 20_array_of_objects1/arrayOfObjects_exo5.js
+++ 20_array_of_objects1/arrayOfObjects_exo5.js
@@ -3,11 +3,38 @@ Complete the getTotalSales function such that it returns the total value of sale
 When a user is subscribed, you will get a subscription object which contains the value. Otherwise, the subscription object will not be present.
  */
 const users = [
-    {id: 1, name: "Alex"},
-    {id: 2, name: "Sam", subscription: {info: {value: 59}}},
-    {id: 3, name: "Charlie", subscription: {info: {value: 31}}}
+    {
+        id: 1,
+        name: "Alex"
+    },
+    {
+        id: 2,
+        name: "Sam",
+        subscription:
+            {
+                info:
+                    {
+                        value: 59
+                    }
+            }
+            },
+    {
+        id: 3,
+        name: "Charlie",
+        subscription:
+            {
+                info:
+                    {
+                        value: 31
+                    }
+            }
+    }
 ];
 const getTotalSales = someUser=>{
-
+    let sum = 0;
+    someUser.forEach(t=>{
+        sum = sum + t.subscription?.info?.value;
+    })
+    return sum;
 };
 console.log(getTotalSales(users))
