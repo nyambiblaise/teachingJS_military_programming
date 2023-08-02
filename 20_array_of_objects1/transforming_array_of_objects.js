378155350ebe3800defc04be1262fce16a881795
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-02 07:49:59 -0400

    arrays of objects 1

--- 20_array_of_objects1/transforming_array_of_objects.js
+++ 20_array_of_objects1/transforming_array_of_objects.js
@@ -4,3 +4,5 @@ const convertNameToUpperCase = someNames=>{
     return someNames.map(name =>name.toUpperCase());
 }
 console.log(convertNameToUpperCase(names))
+
+//The .map() works similarly for arrays of objects. You can transform an array of objects into a new array of objects, or, you can transform it into an array of strings or an array of numbers. This allows you to extract some properties from an array of objects. Here's an example:
