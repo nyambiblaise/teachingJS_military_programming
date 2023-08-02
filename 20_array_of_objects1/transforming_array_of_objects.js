bebfd5b4c0ce4b95b269663910dddd6f297f221a
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-02 07:51:56 -0400

    arrays of objects 1

--- 20_array_of_objects1/transforming_array_of_objects.js
+++ 20_array_of_objects1/transforming_array_of_objects.js
@@ -6,3 +6,19 @@ const convertNameToUpperCase = someNames=>{
 console.log(convertNameToUpperCase(names))
 
 //The .map() works similarly for arrays of objects. You can transform an array of objects into a new array of objects, or, you can transform it into an array of strings or an array of numbers. This allows you to extract some properties from an array of objects. Here's an example:
+const tweets = [
+    {
+        id: 1080777336298049537,
+        message: "Hello Twitter 👋",
+        created_at: "2023-08-03 11:46:00"
+    },
+    {
+        id: 1080777336298195435,
+        message: "How do you keep track of your notes?",
+        created_at: "2023-08-19 15:32:00"
+    }
+];
+const getTweets = allTweets=>{
+    return allTweets.map(allTweets=>allTweets.message);
+}
+console.log(getTweets(tweets));
