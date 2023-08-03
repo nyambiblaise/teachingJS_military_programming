c7c700e0ff4e4e59037c9143a302190c5bbf224a
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-03 18:19:57 -0400

    arrays of objects 1

--- 20_array_of_objects1/transforming_array_of_objects.js
+++ 20_array_of_objects1/transforming_array_of_objects.js
@@ -20,4 +20,4 @@ const tweets = [
 const getTweets = allTweets=>{
     return allTweets.map(allTweets => allTweets.message);
 }
-console.log(getTweets(tweets));
+console.log(getTweets(tweets).message);
