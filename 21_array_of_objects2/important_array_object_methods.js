0a32c7a71c1c9a225df3ea63b2ea47fa1040b812
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-03 20:33:19 -0400

    arrays of objects 2

---
+++ 21_array_of_objects2/important_array_object_methods.js
@@ -0,0 +1,8 @@
+
+Calling the .filter() method on an array of objects will return an array containing the objects that pass the condition you specify in the callback.
+
+Calling the .find() method on an array of objects will return the first object that matches the condition you specify in the callback, or undefined if no objects satisfy the condition.
+
+Calling the .some() method on an array of objects will return true when at least one item in the array satisfies the condition you specified in the callback. Otherwise, it returns false.
+
+Calling the .every() method on an array of objects will return true when every item in the array satisfies the condition you specified in the callback. Otherwise, it returns false.
