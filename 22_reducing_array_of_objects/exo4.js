e76258e5cca172a067e864e9b932813bbda21e12
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-13 19:43:10 -0400

    reducing arrays of objects

---
+++ 22_reducing_array_of_objects/exo4.js
@@ -0,0 +1,11 @@
+//Complete the getProduct function such that it returns the product (multiplication) of the value in every recording.
+const sampleRecordings = [{
+    value: 2,
+}, {
+    value: 5
+}];
+const findProducts = (x) => {
+  return x.reduce((t,c)=>{
+      return t * c.value;
+  },1);
+}
