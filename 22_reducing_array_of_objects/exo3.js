4a32fe36808b0eca9604db9604b157e6f5207aca
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-13 19:40:04 -0400

    reducing arrays of objects

---
+++ 22_reducing_array_of_objects/exo3.js
@@ -0,0 +1,14 @@
+//Complete the getCartTotal function such that it returns the total value of the cart. This can be calculated by multiplying the price by the quantity and returning its sum.
+const sampleProducts = [{
+    price: 10,
+    quantity: 3
+}, {
+    price: 5,
+    quantity: 4
+}]
+const findTotal = (items) => {
+  return items.reduce((total,current)=>{
+      return total + (current.price * current.quantity);
+  },0)
+}
+console.log()
