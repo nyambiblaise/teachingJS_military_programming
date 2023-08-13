1cb5ffe32bf43ba8d2533461d2ae726f25f52d3e
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-12 23:20:39 -0400

    reducing arrays of objects

--- 22_reducing_array_of_objects/introduction.js
+++ 22_reducing_array_of_objects/introduction.js
@@ -2,6 +2,7 @@
  grasp because you have to keep track of several variables in your head and these variables now have different data types (the current will now be an object).
 
  Reduce refresher
-The reduce() method is used to calculate a single value from an array. In other terms, the reduce() method reduces an array into a single value.
+The reduce() method is used to calculate a single value from an array. In other terms, the reduce() method reduces
+ an array into a single value.
 The most common use cases of reduce are sum & multiplication .
  */
