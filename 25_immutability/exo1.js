70fd38ac0848d4cebb8986d5cf2bebd4a7e7f789
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-13 20:57:14 -0400

    immutability

---
+++ 25_immutability/exo1.js
@@ -0,0 +1,9 @@
+//Complete the function incrementAge such that it adds 1 to the current age from the user parameter it receives.
+const sampleUser = {
+    id: 1,
+    age: 23
+};
+const incrementAge = user => {
+
+}
+console.log(incrementAge(sampleUser));
