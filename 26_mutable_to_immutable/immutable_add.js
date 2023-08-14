a3b135ce327e525383a3304a1c8c9e3d212bd28f
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-13 21:43:42 -0400

    mutable to immutable

--- 26_mutable_to_immutable/immutable_add.js
+++ 26_mutable_to_immutable/immutable_add.js
@@ -5,3 +5,5 @@ const grades = [10, 20];
 const newGrades = [...grades,30];
 console.log(grades);
 console.log(newGrades);
+
+//The reason why this works is that you're creating a new array with the [ ] syntax, and inside this array, you're spreading the items from grades and adding to it a new value which is 15.
