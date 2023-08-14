4d7fad29ec991a91d57418044dc8b80c0daa18d5
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-13 23:17:23 -0400

    class instance methods

---
+++ 28_class_instance_methods/exo2_todo_app_OOP.js
@@ -0,0 +1,34 @@
+/*
+In this project, you will write the class that powers a Todo app.
+We've already provided an empty class Todos.
+
+You need to implement 7 instance methods so that the app works. As soon as you've got the first 2 methods implemented, you will be able to test them out in the browser tab.
+
+1. getAll()
+This method should return all the todos.
+
+2. getCount()
+This method should return a number representing the number of todos.
+
+After implementing these 2 methods, you will be able to see the 2 sample todos rendered in the browser tab. Try inserting a new todo using the form. It will fail until you implement the next method.
+
+3. add(title, category)
+This method receives the title (string) and the category (string). It should add a new object into the existing array of objects, following the same format.
+Once this method is implemented, you will be able to add todos using the form.
+
+4. getWork()
+This method should return an array of objects containing only the todos that have a work category.
+The format of the objects inside the array should stay the same.
+
+5. getWorkCount()
+This method should return a number representing how many todos have the work category. try to keep your code DRY (Don't Repeat Yourself).
+
+You will now be able to filter todos by work category in the browser tab.
+
+6. getPersonal()
+This method should return an array of objects containing only the todos that have a personal category.
+The format of the objects inside the array should stay the same.
+
+7. getPersonalCount()
+This method should return a number representing how many todos have the personal category. try to keep your code DRY (Don't Repeat Yourself).
+ */
