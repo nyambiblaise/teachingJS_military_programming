4c0e627055de0d428d7c292f6349a5d968547715
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-13 21:53:23 -0400

    mutable to immutable

---
+++ 26_mutable_to_immutable/exo2.js
@@ -0,0 +1,8 @@
+/*
+Complete the function addApp such that it immutably adds the app parameter to the apps array it receives.
+This means that the original array should not be modified.
+ */
+const originalApps = ["Calculator", "Phone"];
+const addApp = (apps,app)=>{
+
+}
