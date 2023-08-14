ca8590335dc75915c9b9764ec3cac7f6fa3372c5
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-13 20:03:19 -0400

    semi colon

---
+++ 24_semi_colon/recap.js
@@ -0,0 +1,5 @@
+/*
+JavaScript has semi-colons because it's common to minify the code before publishing the website.
+Automated tools will minify your code. You should never minify it yourself or write minified code.
+Minification works by removing the comments and removing all the blank spaces (thus putting all your code on 1 line).
+ */
