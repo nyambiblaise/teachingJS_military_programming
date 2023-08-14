ec8a272f3687d51117475e14c7d8a7e79174331a
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-13 22:37:00 -0400

    introduction to classes

--- 27_introduction_to_classes/exo2_person_exo.js
+++ 27_introduction_to_classes/exo2_person_exo.js
@@ -1,5 +1,6 @@
 //Complete the function createPerson such that it returns a new instance of the class Person with the correct firstName and lastName instance variables based on the fName and lName parameters that it receives.
+import {Person} from "./person.js";
 
 const createPerson = (fName, lName) => {
-
+    return new Person(fName,lName);
 }
