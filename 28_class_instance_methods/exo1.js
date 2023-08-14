77c6d5ee1e4a7fc8deaa64327a89a5caf04d4987
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-13 22:52:34 -0400

    class instance methods

--- 28_class_instance_methods/exo1.js
+++ 28_class_instance_methods/exo1.js
@@ -5,18 +5,19 @@ getFullName should return the prefix (for example, Mrs) followed by a dot charac
 canVote() should return true when the age is 18 or above, and false otherwise.
  */
 class User{
-    constructor(firstName,lastName,age) {
+    constructor(firstName,lastName,status,age) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
+        this.status = status;
     }
     getFullName(){
-        return `Mr. ${this.firstName} ${this.lastName}`
+        return `${this.status}. ${this.firstName} ${this.lastName}`
     }
     canVote(){
         if(this.age>=18)
             return true;
     }
 }
-const newUser = new User("Mary","Ashu",90);
+const newUser = new User("Mary","Ashu","Mrs",90);
 console.log(newUser.getFullName());
