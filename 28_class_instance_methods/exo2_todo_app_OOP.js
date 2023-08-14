ed995107333d01591021b904bd9befeff498a6f8
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-13 23:17:58 -0400

    class instance methods

--- 28_class_instance_methods/exo2_todo_app_OOP.js
+++ 28_class_instance_methods/exo2_todo_app_OOP.js
@@ -32,3 +32,40 @@ The format of the objects inside the array should stay the same.
 7. getPersonalCount()
 This method should return a number representing how many todos have the personal category. try to keep your code DRY (Don't Repeat Yourself).
  */
+class Todos {
+    constructor() {
+        this.todos = [{
+            title: "Learn JavaScript",
+            category: "work"
+        }, {
+            title: "Meditate",
+            category: "personal"
+        }];
+    }
+    getAll(){
+        return this.todos;
+    }
+    getCount(){
+        return this.todos.length;
+    }
+    add(title,category){
+        this.todos.push({title,category});
+        return this.todos;
+    }
+    getWork(){
+        return this.todos.filter((someTd)=>{
+            return someTd.category==="work"
+        });
+    }
+    getWorkCount(){
+        return this.getWork().length;
+    }
+    getPersonal(){
+        return this.todos.filter((someTd)=>{
+            return someTd.category==="personal"
+        });
+    }
+    getPersonalCount(){
+        return this.getPersonal().length;
+    }
+}
