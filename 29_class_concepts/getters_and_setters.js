f39f844fa3683540f8421d4a438932cbc72259f9
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-14 21:52:56 -0400

    getters and setters

---
+++ 29_class_concepts/getters_and_setters.js
@@ -0,0 +1,14 @@
+/*
+You can still use getters and setters with the pattern where you capture constructor parameters. That's because, whenever you access this.age from within the class, it will also automatically trigger the set age(value) function. This is why we had to create an internal property and call it this._age to avoid creating an infinite loop.
+ */
+class User2{
+    constructor(myAge) {
+        this.myAge = myAge;
+    }
+    set setAge(someAge){
+        this._myAge = someAge;
+    }
+    get getAge(){
+        return this._myAge;
+    }
+}
