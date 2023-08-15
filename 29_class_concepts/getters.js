a72e75288028de2a453a3f26564373847e0786c0
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-14 21:49:56 -0400

    getters and setters

--- 29_class_concepts/getters.js
+++ 29_class_concepts/getters.js
@@ -4,13 +4,13 @@ The remaining problem is that if we try to access user.age, we get undefined. Th
 Let's go ahead and define a getter for the age property. We do that by defining a get age() function as follows:
  */
 class User1{
-    set age(value){
+    set setAge(value){
         this._age = Number.parseInt(value,10);
     }
-    get age(){
+    get getAge(){
         return this._age;
     }
 }
 const user = new User1();
-user.age = 12;
-console.log()
+user.setAge = "12";
+console.log(user.getAge)
