c10e554c07c662e1708c44c8d9742c6c8907270b
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-14 21:39:46 -0400

    getters and setters

--- 29_class_concepts/getters_and_setters.js
+++ 29_class_concepts/getters_and_setters.js
@@ -3,3 +3,9 @@ Defining getters and setters in a class is mostly used to validate or modify cer
 
 Let's take an example where we'd like to create a class User that has an age property. However, we'd like to always make sure that this age property is a number. For that, we define a setter for the age property. Here's what the setter looks like:
  */
+class User{
+    set age(value){
+        console.log("some age");
+        this._age = Number.parseInt(value,10);
+    }
+}
