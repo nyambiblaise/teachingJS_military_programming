f1a984876b4a053b597829c7a448885e16fb7018
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-14 21:59:40 -0400

    getters and setters

---
+++ 29_class_concepts/exo1.js
@@ -0,0 +1,10 @@
+/*
+Implement a getter for the todos property in the Tasks class.
+This getter should return a CSV string of the todos. So each todo should be separated by a comma and space character.
+ */
+class Task{
+    constructor(todos) {
+        this._todos = todos;
+    }
+
+}
