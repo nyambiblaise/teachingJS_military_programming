38d3dfda5345612c3ba9761efbe0fafafc1fd802
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-20 23:02:30 -0400

    prototypal inheritance

--- 31_prototypal_inheritance/class_vs_function.js
+++ 31_prototypal_inheritance/class_vs_function.js
@@ -25,11 +25,13 @@ Rectangle1.prototype.isArea = function () {
     return this.width * this.length;
 }
 /*
-You can think of the prototype as an object that contains all the instance methods that a certain function can have. This prototype is linked in what we call the prototype chain. This is explained in the next lesson.
+You can think of the prototype as an object that contains all the instance methods that a certain function can have.
+ This prototype is linked in what we call the prototype chain. This is explained in the file prototype_chain.js
 
 So by assigning a new function to Rectangle.prototype you are adding a new instance method to the instances of Rectangle.
 That means, every time you create a new Rectangle, that instance will have an instance method called isSquare which can access the instance properties via this. (for example, this.width and this.length).
 
-Again, the syntax is not the best, but there is a reason behind it, which you'll learn about in the next lesson!
+String.prototype.trim().
+What this means is that trim() is a method that can be called on instances of String. This means, trim() can be called on any new string that you create.
  */
 console.log(new Rectangle1(10,4).isArea());
