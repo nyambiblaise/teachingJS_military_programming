b92d6b454d0d97b78b60d6f48233ca3450d27c9a
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-20 23:17:31 -0400

    prototypal inheritance

---
+++ 31_prototypal_inheritance/prototypal_inheritance.js
@@ -0,0 +1,4 @@
+/*
+Prototypal inheritance is here to solve the problem in traditional inheritance whereby when you want to inhreit from
+ a class, all the method of that class are inherited. Here, you may opt to inherit just specific methods in a class.
+ */
