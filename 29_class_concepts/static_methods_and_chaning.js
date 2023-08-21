651e71efb35a9df8585bfc33675eca4df99de9d6
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-20 20:58:01 -0400

    class concepts

--- 29_class_concepts/static_methods_and_chaning.js
+++ 29_class_concepts/static_methods_and_chaning.js
@@ -1,4 +1,5 @@
 //static methods are methods that are called direcly without instantiating the class.
+//in a static method, we cannot use the "this" keyword.
 class Student{
     static showInfo(){
         console.log("Welcome Student");
