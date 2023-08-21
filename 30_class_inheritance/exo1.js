c7ff977bb382133e79c7b378838d7a189095c65b
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-20 21:45:13 -0400

    inheritance

---
+++ 30_class_inheritance/exo1.js
@@ -0,0 +1,12 @@
+/*try to reproduce the classes and methods to obey the code below
+
+const user = new User("Sam", "Green", 17);
+user.canVote(); // false
+user.getFullName(); // "Sam Green"
+
+const admin = new Admin("Alex", "Blue", 20);
+admin.canVote(); // true
+admin.getFullName(); // "Alex Blue [admin]"
+admin.updateConfig(); // "Config updated"
+
+ */
