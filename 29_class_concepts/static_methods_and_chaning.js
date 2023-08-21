b77606df21923622ea8895007074df157dd0db65
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-20 21:07:48 -0400

    class concepts

--- 29_class_concepts/static_methods_and_chaning.js
+++ 29_class_concepts/static_methods_and_chaning.js
@@ -10,14 +10,19 @@ Student.showInfo();
 //Method chaining
 // You may sometimes encounter method chaining, which is a method called on the result of another method. For example, assuming a class Course:
 class Course{
+    constructor(status,grade,certificate) {
+        this.status = status;
+        this.grade = grade;
+        this.certificate = certificate;
+    }
     markAsComplicated(){
-        console.log(`Completed`);
+        return this.status;
     }
     setGrade(g){
-        console.log(`Grade is ${g}`);
+        return this.grade = `Grade is ${g}`;
     }
     requestCertificate(){
-        console.log(`Certificate is Requested`);
+        this.certificate = `Certificate is Requested`;
     }
 }
 //with chaining, it is possible to do....
@@ -33,4 +38,4 @@ Notice how we call setGrade(40) on the result of .markAsCompleted(). Similarly, 
  For this to work, these instance methods should always return this. The reference to the current instance. By doing so, you are able to call other methods on the instance because they're returning the current instance.
  */
 console.log("\n METHOD CHAINING");
-course.markAsComplicated().setGrade(10).requestCertificate();
+//course.markAsComplicated().setGrade(10).requestCertificate();
