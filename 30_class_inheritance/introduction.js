74467a30ddd90317443546dcecde4ecf067cf0a3
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-20 21:39:41 -0400

    inheritance

--- 30_class_inheritance/introduction.js
+++ 30_class_inheritance/introduction.js
@@ -1,5 +1,6 @@
 /*
 An important principle in programming is called DRY (Don't Repeat Yourself).
+In JavaScript, we inherit using the extend keyword
 
 When describing the real world with classes, you will notice sometimes that some classes are very similar. In fact, some methods would be exactly the same thing (maybe with different variable names). For example, let's say you're creating classes for your company where you've got an employee and a manager. Here's how they would look:
  */
