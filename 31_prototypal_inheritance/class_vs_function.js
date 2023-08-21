17246f836c6cf291b9f8fe3b2bd4fd401974cf02
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-20 22:48:39 -0400

    prototypal inheritance

---
+++ 31_prototypal_inheritance/class_vs_function.js
@@ -0,0 +1,16 @@
+/*
+JavaScript doesn't have real classes! 😨 The class syntax you're seeing is called syntactic sugar which is a fancy term for saying that it's a syntax that makes your code more readable
+
+ */
+class Rectangle {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    isSquare() {
+        return this.width === this.height;
+    }
+}
+//In fact, if you ask JavaScript about the type of Rectangle, you'll get function:
+console.log(typeof(Rectangle));
