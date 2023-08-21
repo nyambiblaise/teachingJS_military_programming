00426d2e84074b889001ae10c1585deabdcbf4bd
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-20 23:59:12 -0400

    public and private class fields

--- 33_public_and_private_fields/private_fields.js
+++ 33_public_and_private_fields/private_fields.js
@@ -1,7 +1,9 @@
 //private class members cannot be accessed out of the class in which they were created
 //To mark an instance variable or method as private, you have to prefix it with the # (hash sign).
 class User{
-    age = 16;
+    #age = 16; //private
+    name = "Paul"; //public
 }
 const user = new User();
-console.log(user.age);
+console.log(user.age); //undefined because age is private
+console.log(user.name);
