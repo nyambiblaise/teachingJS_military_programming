fc17ea4904e3e51d06b4611a23e90657e4a957b4
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-20 22:00:24 -0400

    inheritance

--- 30_class_inheritance/super_property.js
+++ 30_class_inheritance/super_property.js
@@ -1,5 +1,5 @@
 /*
-
+The super keywords is used to call methods on the parent class.
  */
 class Employee{
     constructor(fName, lName, age) {
@@ -15,13 +15,26 @@ class Employee{
     }
 }
 class Manager extends Employee{
+
+    /* This line will give an error because we already have this constructor
+    constructor(firstName, lastName, department) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.department = department;
+    }
+    So, to solve the issue, we use the super keyword
+    */
+    constructor(firstName, lastName, department) {
+        super(firstName,lastName,department);
+        this.department = department;
+    }
     getFullNames() {
-        return `${this.fName} ${this.lName} (Manager)`;
+        return `${this.fName} ${this.lName} working at ${this.department} As (Manager)`;
     }
     deleteEmployee(employeeName){
         return `Employee with name ${employeeName} has been deleted`;
     }
 }
-const manager = new Manager("joshua","silas",90);
+const manager = new Manager("joshua","silas","IT");
 console.log(manager.getFullNames());
 console.log(manager.getInitials());
