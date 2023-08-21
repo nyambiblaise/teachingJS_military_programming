f1495add1443e4c1fcad0ae2f129149a82726759
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-20 20:53:48 -0400

    getters and setters

---
+++ 29_class_concepts/static_methods_and_chaning.js
@@ -0,0 +1,7 @@
+//static methods are methods that are called direcly without instantiating the class.
+class Student{
+    static showInfo(){
+        console.log("Welcome Student");
+    }
+}
+Student.showInfo();
