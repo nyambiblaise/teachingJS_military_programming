83121889cd60bd7df229e54e321da75cce46aff2
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-20 23:28:17 -0400

    prototypal inheritance

--- 31_prototypal_inheritance/prototypal_inheritance.js
+++ 31_prototypal_inheritance/prototypal_inheritance.js
@@ -3,6 +3,9 @@ Prototypal inheritance is here to solve the problem in traditional inheritance w
  a class, all the method of that class are inherited. Here, you may opt to inherit just specific methods in a class.
 
  Let us assume we have the following classes and their methods:
+ Problem: You ask for Banana, but you end up having a Gorilla holding a Banana and the entire Jungle.
+ Here, Banana inherits from Gorilla who inherits from the Jungle.
+
  Jungle
  - rain()
  - growTree();
@@ -14,4 +17,27 @@ Prototypal inheritance is here to solve the problem in traditional inheritance w
 Banana
 - peel()
 - grow()
+
+We want to have just the Banana.peel() method and the Gorilla.eat()
+To solve this problem, we add the prototype to the function
  */
+function Jungle(){
+
+}
+function Gorila(){
+
+}
+function Banana() {
+
+}
+Banana.prototype.peel = function () {
+    
+}
+Gorila.prototype.eat = function () {
+
+}
+// Extend our GorillaBanana with the Gorilla's eat() method =>Inheritance here
+BananaGorila.prototype.eat = Gorila.prototype.eat;
+
+// Extend our GorillaBanana with Banana's peel() method => Inheritance here
+BananaGorila.prototype.peel = Banana.prototype.peel;
