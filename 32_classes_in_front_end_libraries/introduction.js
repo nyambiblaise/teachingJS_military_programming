3aebf3e3d0a3bc2e9b5e13bacb2febab12ddc305
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-20 23:36:05 -0400

    classes in front-end libraries

---
+++ 32_classes_in_front_end_libraries/introduction.js
@@ -0,0 +1,17 @@
+/*
+This chapter will show class usage examples in front-end libraries/frameworks.
+This chapter won't explain the libraries/frameworks and how they work but rather show you how classes are being used to create reusable components. If you've never used these libraries/frameworks, you can just glance over the lessons and come back to it later if necessary.
+
+The goal of it is to show you how classes are used in real life. Note, that the examples are not guaranteed to be 100% correct or up to date. Instead, think of them a bit as pseudo-code.
+
+We'll cover the following libraries/frameworks:
+
+React (without hooks)
+Angular
+Web Components
+Lit (two small introductory chapters are available at the end of this course).
+You can decide to read the lessons that you think will be relevant to you. At the end of the chapter, we'll have a "Fake" front-end library to show you the gist of how things work.
+
+Not useful
+
+ */
