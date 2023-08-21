4b3f43bf6ef07341021611558373d3da458573ac
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-20 23:53:17 -0400

    public and private class fields

---
+++ 33_public_and_private_fields/introduction.js
@@ -0,0 +1,10 @@
+//Public class fields allows you to define instance variables on a class without having to override the constructor.
+class User{
+    constructor() {
+        this.age = 10;
+    }
+}
+//using public fields...
+class User1{
+    age = 19; //age will be automatically set to 19 withiut using the constructor
+}
