e99a0bee317c0f8f5165a055970f50b9a271bfc5
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-20 21:45:53 -0400

    inheritance

--- 30_class_inheritance/exo1.js
+++ 30_class_inheritance/exo1.js
@@ -10,3 +10,36 @@ admin.getFullName(); // "Alex Blue [admin]"
 admin.updateConfig(); // "Config updated"
 
  */
+
+
+
+//SOLUTION
+class User{
+    constructor(fname,lname,age){
+        this.fname = fname;
+        this.lname = lname;
+        this.age = age;
+    }
+    canVote(){
+        return this.age>=18;
+    }
+    getFullName(){
+        return `${this.fname} ${this.lname}`;
+    }
+}
+class Admin extends User{
+    getFullName(){
+        return `${this.fname} ${this.lname} [admin]`;
+    }
+    updateConfig(){
+        return `Config updated`;
+    }
+}
+
+const user = new User("Sam", "Green", 17);
+console.log(user.canVote()); // false
+console.log(user.getFullName()); // "Sam Green"
+const admin = new Admin("Alex", "Blue", 20);
+console.log(admin.canVote()); // true
+console.log(admin.getFullName()); // "Alex Blue [admin]"
+console.log(admin.updateConfig()); // "Config updated"
