8c9f2dc97e321250f301ce6ebaeedb5bd6e54ecd
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-22 00:07:31 -0400

    promise rejection

--- 37_promise_rejection/promise_with_finally.js
+++ 37_promise_rejection/promise_with_finally.js
@@ -4,8 +4,10 @@ This means that .finally() will execute for both success and error states.
  */
 getWeatherIn("Tokyo").then(data=>{
     console.log(data);
+    console.log("Done fetching weather data")
 }).catch(error=>{
     console.log(error);
+    console.error("Something went wrong");
 }).finally(res=>{
     console.log(res);
 })
