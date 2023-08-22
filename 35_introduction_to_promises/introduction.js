da4db8f17422646c861d5fd6993169190eb80730
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-21 21:32:49 -0400

    introduction to promises

---
+++ 35_introduction_to_promises/introduction.js
@@ -0,0 +1,3 @@
+/*
+We saw in the previous chapter the callback pattern isn't very elegant. This is why promises were created. We'll spend some time working with promises first, and then we'll learn how to create your own promises.
+ */
