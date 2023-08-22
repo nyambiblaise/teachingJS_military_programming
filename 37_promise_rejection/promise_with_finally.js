6e948107d8649a80207984e434d44790f58f9b54
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-22 00:09:24 -0400

    promise rejection

--- 37_promise_rejection/promise_with_finally.js
+++ 37_promise_rejection/promise_with_finally.js
@@ -4,10 +4,9 @@ This means that .finally() will execute for both success and error states.
  */
 getWeatherIn("Tokyo").then(data=>{
     console.log(data);
-    console.log("Done fetching weather data")
 }).catch(error=>{
     console.log(error);
-    console.error("Something went wrong");
-}).finally(res=>{
-    console.log(res);
+}).finally(()=>{
+    console.log("Done fetching weather data")
 })
+//The .finally(callback) will execute after the .then() when the promise resolves successfully and after the .catch() when the promise rejects.
