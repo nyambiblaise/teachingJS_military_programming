1f3ce7b239a6e70d5e9a2d9144a54735c79af1bb
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-21 23:45:25 -0400

    promise rejection

---
+++ 37_promise_rejection/introduction.js
@@ -0,0 +1,9 @@
+/*
+since things doesnt always go as planned, a promise can fail. when such occurs, it is called a rejection.
+
+For example, let's say you're trying to connect to a weather service/API, this might fail because of a network connection issue. In that case, the promise will not resolve successfully (it will not be fulfilled). Instead, it will be rejected.
+
+This lets us know that something went wrong which allows us to handle the error by showing an error message or a retry button for example.
+
+In this case, we say that the promise rejected. The reason for this naming will become clearer once we learn about the Promise.reject() method.
+ */
