2d7b0dc9de9a1cfc8b9d1285182ddc35f4a33eac
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-22 00:33:13 -0400

    promise rejection

---
+++ 37_promise_rejection/promise_all.js
@@ -0,0 +1,5 @@
+/*
+Promise.all returns a Promise that fulfills when all the promises it receives have been fulfilled. It rejects when one of these promises fails. This allows you to execute more than one promise at the same time and wait until all of them have fulfilled.
+
+For example, this could be useful if you need to load a JavaScript file and a CSS file before rendering a component and you don't want to render it before both files have loaded.
+ */
