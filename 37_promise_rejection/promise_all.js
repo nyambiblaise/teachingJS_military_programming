26bfc2b931eb4752489aba9de9c96812eddf246a
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-22 00:33:21 -0400

    promise rejection

--- 37_promise_rejection/promise_all.js
+++ 37_promise_rejection/promise_all.js
@@ -3,3 +3,8 @@ Promise.all returns a Promise that fulfills when all the promises it receives ha
 
 For example, this could be useful if you need to load a JavaScript file and a CSS file before rendering a component and you don't want to render it before both files have loaded.
  */
+Promise.all([promise1, promise2]).then(values => {
+    console.log(values); // [promise1Result, promise2Result]
+}).catch(error => {
+    console.error(error);
+});
