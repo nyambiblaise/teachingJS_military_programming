f910263d509c18de4cecef6a6d910d430f0c6325
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-21 23:42:33 -0400

    using promises

---
+++ 36_using_promises/using_promise_recap.js
@@ -0,0 +1,9 @@
+/*
+A promise can have 3 states: pending, fulfilled, and rejected.
+Every promise starts with the pending state and then becomes fulfilled when it has been completed successfully.
+Promises let you run a callback sometime in the future when the promise has been completed successfully.
+When a promise completes successfully, you will often hear/read people say that it has resolved.
+One of the most useful features of promises is when they resolve with some data, an answer of some computation or some values coming from an external service/API.
+You are not able to extract the value resolved by a promise outside of a promise.
+Don't rush into learning async/await yet. You have to have solid experience in promises first.
+ */
