2a389e3b6215dfc96e35408b5492c9133e4d0ae7
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-21 22:39:02 -0400

    using promises

--- 36_using_promises/introduction.js
+++ 36_using_promises/introduction.js
@@ -4,4 +4,14 @@ pending, fulfilled and rejected
 When you create a promise, it will start in the pending state. When it has been completed successfully, then it becomes in the fulfilled state.
 
 So, when the promise becomes fulfilled, the callback passed to .then(callback) will execute.
+
+Visualizing the pending state
+Assuming a function wait(milliseconds) that returns a promise. Let's try to console.log() the result of wait(1_000):
  */
+const wait = milliseconds=>{
+    return new Promise(()=>{
+        setTimeout(resolve()=>{
+            resolve();
+        },milliseconds);
+    })
+}
