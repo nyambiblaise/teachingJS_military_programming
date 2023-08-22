d015adbda69c6957390cb06ce47377e5b07baa92
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-21 20:52:35 -0400

    asynchronous callbacks

---
+++ 34_asynchronous_callbacks/callback_data_and_handling_errors.js
@@ -0,0 +1,3 @@
+/*
+The success callback is able to receive some arguments. For example, let's say there's an expensive calculation happening and you'd like to make it possible for the success callback to receive the answer of that calculation.
+ */
