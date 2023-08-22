2ae7e0f757f009dd45e5b16348a8eb4853853217
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-21 22:31:10 -0400

    introduction to promises

--- 36_using_promises/introduction.js
+++ 36_using_promises/introduction.js
@@ -1,3 +1,7 @@
 /*
+A promise can have 3 states:
+pending, fulfilled and rejected
+When you create a promise, it will start in the pending state. When it has been completed successfully, then it becomes in the fulfilled state.
 
+So, when the promise becomes fulfilled, the callback passed to .then(callback) will execute.
  */
