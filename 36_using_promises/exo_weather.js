a9646cff3f5d2ec8505e6bcc705904514d909b7d
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-21 23:33:29 -0400

    using promises

--- 36_using_promises/exo_weather.js
+++ 36_using_promises/exo_weather.js
@@ -4,4 +4,15 @@ Take a look at the getWeatherDescription function (in exo_weather_question.js) t
 
 Implement the logWeatherDescription such that it uses getWeatherDescription to log the weather description to the console.
 Look at the sample usage to see the expected result.
+logWeatherDescription("Amsterdam"); //will log "Cloudy"
+logWeatherDescription("Tokyo"); // will log "Sunny"
  */
+
+import {getWeatherDescription} from "./exo_weather_question.js";
+
+const logWeatherDescription = cityName=>{
+
+}
+logWeatherDescription("tokyo");
+logWeatherDescription("douala");
+logWeatherDescription("amsterdam");
