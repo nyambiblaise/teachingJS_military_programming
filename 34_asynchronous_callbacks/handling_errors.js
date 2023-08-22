ad3425fb11780833b1437f2992fb7ed437ee89f4
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-21 21:09:18 -0400

    asynchronous callbacks

--- 34_asynchronous_callbacks/handling_errors.js
+++ 34_asynchronous_callbacks/handling_errors.js
@@ -1,3 +1,13 @@
 /*
+Things get even more complicated when you want to handle errors. Some functions might break, for example, if you're trying to get the weather from an API/service, this might break because of a bad Internet connection, or maybe the weather service is down, etc.
 
+Using the callback pattern, we often see the error callback specified after the success callback.
  */
+const temperatures = [10, 5, 3];
+setTemperatures(temperatures,value=>{
+    console.log(value); //(the sum of temperatures)
+},reason=>{ //// this callback will run when there's an error
+    console.error(reason);
+})
+//the simplified function can be
+setTemperatures(temperatures,successCallBack,failureFallBack);
