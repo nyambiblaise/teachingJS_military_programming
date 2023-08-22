d294fcecf5317dacd77cfa3962e8b0b1d58bdf0e
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-21 21:49:04 -0400

    introduction to promises

--- 35_introduction_to_promises/introduction.js
+++ 35_introduction_to_promises/introduction.js
@@ -12,3 +12,11 @@ const time = 1000;
 wait(time).then(()=>{
     console.log(`Waited for ${time/1000} seconds`);
 })
+/*
+Because the function wait() returns a new promise, then we can call .then() on its result.
+You can only call .then() on functions that return a new Promise.
+
+Several Web APIs (functions in the browser) work with promises, which is why this topic is important. Most importantly:
+fetch (explained in a later chapter)
+getUserMedia (access camera/audio)
+ */
