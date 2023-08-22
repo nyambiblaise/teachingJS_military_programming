568ec18aeacf67ba28084f42e20e9784c62704fb
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-21 20:30:02 -0400

    asynchronous callbacks

---
+++ 34_asynchronous_callbacks/exo1.js
@@ -0,0 +1,7 @@
+//create a function called  delayedWelcome function such that it delays the console.log call 1 second into the
+// future.
+
+const delayedWelcome = name=>{
+    console.log(`Welcome ${name}`);
+};
+setTimeout(delayedWelcome,1000);
