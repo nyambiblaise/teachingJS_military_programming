c08a62103971cf45c227ef9f52e028f2156198f2
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-21 21:45:18 -0400

    introduction to promises

--- 35_introduction_to_promises/introduction.js
+++ 35_introduction_to_promises/introduction.js
@@ -1,3 +1,10 @@
 /*
 A Promise is a JavaScript feature that allows us to schedule work in the future and then runs callbacks based on the outcome of the promise (whether it was successful or not).
  */
+const wait = milliseconds=>{
+    return new Promise(resolve => {
+        setTimeout(()=>{
+            resolve();
+        },milliseconds);
+    });
+}
