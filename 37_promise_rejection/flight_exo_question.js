820b6f55b17b2d3f42d5d21e59283a3d815f8f58
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-21 23:56:44 -0400

    promise rejection

--- 37_promise_rejection/flight_exo_question.js
+++ 37_promise_rejection/flight_exo_question.js
@@ -1,12 +1,8 @@
-/**
- * @param {number} milliseconds
- */
 export const fakeFetch = (endpoint) => {
     return new Promise((resolve, reject) => {
         if (endpoint !== "flight-status") {
             reject("Invalid endpoint. Only flight-status is supported.")
         }
-        // https://codetogo.io/how-to-get-random-number-between-two-numbers-in-javascript/
         const min = 1;
         const max = 2;
         const rand = Math.floor(Math.random() * (max - min + 1)) + min;
---
+++ 37_promise_rejection/flight_exo_solution.js
@@ -0,0 +1,7 @@
+/*
+Use the fakeFetch function from flight_exo_question.js that takes a string as a parameter. This fake fetch only
+ handles the "flight-status" for now. However, we want you to use it to get the "user-details".
+This will fail at the moment since it's not implemented so make sure to handle the rejected case.
+
+In the future, once we implement user-details, the code will automatically work because the promise will not reject.
+ */
