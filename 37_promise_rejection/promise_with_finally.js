11a8c1d6c536c9fca4cdcf35240321c8f5240061
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-22 00:04:54 -0400

    promise rejection

---
+++ 37_promise_rejection/promise_with_finally.js
@@ -0,0 +1,7 @@
+/*
+NOTE:
+The try...catch statement does not work with promises. That's because the promises are asynchronous meaning that they are happening at a later stage.
+So, if a promise fails, this is going to be sometime in the future. And, by that time, the try...catch statement has already been completed a long time ago.
+
+Also, you don't need try...catch because promises have .then() and .catch() that act similarly to the try...catch statement.
+ */
