d9dce262eedcde73a7c757660d51354ce6570c88
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-21 20:54:11 -0400

    asynchronous callbacks

--- 34_asynchronous_callbacks/callback_data_and_handling_errors.js
+++ 34_asynchronous_callbacks/callback_data_and_handling_errors.js
@@ -1,3 +1,7 @@
 /*
 The success callback is able to receive some arguments. For example, let's say there's an expensive calculation happening and you'd like to make it possible for the success callback to receive the answer of that calculation.
  */
+const temperatures = [10, 5, 3];
+sumTemperatures(temperatures,value=>{
+    console.log(value);
+});
