d4167205c0bbd241c463a7c9c37f522f4decd0f5
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-21 21:39:23 -0400

    introduction to promises

--- 35_introduction_to_promises/introduction.js
+++ 35_introduction_to_promises/introduction.js
@@ -1,3 +1,3 @@
 /*
-We saw in the previous chapter the callback pattern isn't very elegant. This is why promises were created. We'll spend some time working with promises first, and then we'll learn how to create your own promises.
+A Promise is a JavaScript feature that allows us to schedule work in the future and then runs callbacks based on the outcome of the promise (whether it was successful or not).
  */
