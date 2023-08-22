201d4f1cc392bed42511f614eec4586dfe9bc647
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-21 22:59:38 -0400

    using promises

---
+++ 36_using_promises/resolving_data.js
@@ -0,0 +1,6 @@
+/*
+When a promise completes successfully, you will often hear/read people say that it has resolved.
+The reason for this naming will become clearer once we learn about the Promise.resolve() method.
+
+One of the most useful features of promises is when they resolve with some data, an answer of some computation, or some values coming from an external service/API.
+ */
