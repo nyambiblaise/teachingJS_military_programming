ea52018d5ee242cd9517f6a98dfef9b7b1d63fa9
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-22 22:41:08 -0400

    introduction to fetch

---
+++ 42_intro_to_fetch/fetch_recap.js
@@ -0,0 +1,5 @@
+/*
+The fetch API is used to send and/or receive data from a service (an API) without reloading the page.
+The fetch API always returns a promise.
+The fetch API expects a URL as its first parameter: fetch(URL).
+ */
--- 42_intro_to_fetch/introduction.js
+++ 42_intro_to_fetch/introduction.js
@@ -1,17 +1,22 @@
 /*
 We use the fetch Web API (or fetch API) to make a request to a server (which could be ours or an external one, for example, a Weather API) to retrieve some information without reloading the page.
+
+This is extremely important. Fetch always returns a promise.
+This is because fetch has to go to the network, this could take anywhere between a couple of milliseconds and a second (on average). So, we cannot freeze the entire browser while the fetch request is working.
+Thus, by design, fetch returns a promise that we can resolve once the request has finished.
+
+This means that we'll have to add a .then() after the fetch() call.
  */
 const url = "https://jsdemo-3f387-default-rtdb.europe-west1.firebasedatabase.app/notifications/new.json";
 const search = ()=>{
-    return new Promise((resolve,reject)=>{
-        setTimeout(()=>{
-            resolve(fetch(url));
-        },1000);
-        //reject("Something went wrong getting data");
+    return fetch(url).then(()=>{
+        return new Promise((resolve,reject)=>{
+            setTimeout(()=>{
+                resolve();
+            },1000);
+    })
+    }).catch(error=>{
+        console.log(error);
     });
 }
-search().then(data=>{
-    console.log(data);
-}).catch(error=>{
-    console.log(error);
-})
+console.log(search())
