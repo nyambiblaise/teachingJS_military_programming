3179425fe68cbb08fe8889466c65283f16a297b7
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-22 22:54:36 -0400

    introduction to fetch

--- 42_intro_to_fetch/fetching_json.js
+++ 42_intro_to_fetch/fetching_json.js
@@ -1,4 +1,7 @@
 /*
+Here, you will learn how to CONSUME JSON data using fetch. But keep in mind that fetch itself is a generic browser
+ function and is not only meant for reading JSON.
+
 In the previous lesson, we learned that fetch always returns a promise. So, what do we get when we resolve this promise?
 We get a Response object that we're going to call response.
 */
@@ -9,4 +12,10 @@ fetch(URL)
 /*
 So, because fetch(URL) returns a promise, we can resolve the promise with a .then(callback). This callback will run sometime in the future when the fetch request has been completed.
 Also, this callback will receive as a first argument the response from the request that we sent. You can call this argument whatever you want, however, we recommend that you call it response.
+
+This is why whenever you fetch(URL), the response that we get back is just a response object and the browser doesn't know that the data it has received is in JSON format. This is why we should manually convert the response into JSON:
  */
+fetch(URL)
+    .then(response => {
+        return response.json();
+    });
