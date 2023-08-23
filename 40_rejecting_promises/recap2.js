ebe8dc296275e69fce6476c9d901a112bf4ab21d
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-22 22:02:46 -0400

    rejecting promises

---
+++ 40_rejecting_promises/recap2.js
@@ -0,0 +1,25 @@
+/*
+Create fakeFetch function such that it returns a promise that resolves successfully after 1 second with the following
+ object:
+{
+    departed: false,
+    delayed: true
+}
+ */
+const info = {
+    departed:false,
+    delayed:true
+}
+const fakeFetch = (info) => {
+    return new Promise(resolve=>{
+        setTimeout(()=>{
+            resolve({
+                departed:false,
+                delayed:true
+            });
+        },1000);
+    })
+}
+fakeFetch().then((data) => {
+    console.log(data);
+});
