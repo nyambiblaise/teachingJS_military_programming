e81c620dea856aa5d9154401d2ce1a2f16c344b2
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-22 21:25:13 -0400

    creating promises

--- 39_creating_promises/introduction.js
+++ 39_creating_promises/introduction.js
@@ -34,3 +34,5 @@ const waitFourSeconds = ()=>{
         },1000);
     })
 }
+//When you call resolve(), the promise state will be changed from pending to fulfilled. This is why when you call resolve(), the .then() callback will execute.
+console.log(waitFourSeconds());
