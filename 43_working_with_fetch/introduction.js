b0f585417585793a027605dda08d5f118909a1c2
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-22 23:45:18 -0400

    working with fetch

--- 43_working_with_fetch/introduction.js
+++ 43_working_with_fetch/introduction.js
@@ -7,12 +7,23 @@ array: https://jsonplaceholder.typicode.com/todos/1
 const usersURL = "https://jsonplaceholder.typicode.com/users";
 const todoListURL = "https://jsonplaceholder.typicode.com/todos/1";
 
+//getting data from a simple array
 const getToDoListData = ()=>{
     return fetch(todoListURL).then(response=>response.json()).then(data=>{
-        //console.log(data);
+        console.log(data);
         return data;
     })
 }
 getToDoListData().then(value => {
     console.log(value)
 });
+
+//getting data from an array of objects
+const getAllUsersWithInfo = ()=>{
+    return fetch(usersURL).then(res=>res.json()).then(data=>{
+        data.forEach(somedata=>{
+            console.log(somedata);
+        })
+    })
+}
+getAllUsersWithInfo();
