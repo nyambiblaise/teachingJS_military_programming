81d7f93fae58b52cc46f40b921b471bedde8495a
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-22 23:24:10 -0400

    introduction to fetch

--- 42_intro_to_fetch/exo1_fantasy_premier_league_api.js
+++ 42_intro_to_fetch/exo1_fantasy_premier_league_api.js
@@ -1,7 +1,7 @@
 const url = "https://fantasy.premierleague.com/api/entry";
-const getManager = (managerID)=>{
+const getManagerByID = (managerID)=>{
     return fetch(`${url}/${managerID}`).then(response=>response.json()).then(data=>{
         console.log(data);
     })
 }
-getManager(2352989);
+getManagerByID(2352989);
