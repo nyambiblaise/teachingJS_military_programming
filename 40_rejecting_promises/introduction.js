9e18b8a3a309ab8b8cb756cdf0ea0ccf9b846d23
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-22 21:49:48 -0400

    rejecting promises

--- 40_rejecting_promises/introduction.js
+++ 40_rejecting_promises/introduction.js
@@ -1,1 +1,10 @@
-.
+/*
+The promise executor function (the function passed to the new Promise(...) call), in fact, receives 2 arguments. The first one we called resolve and we saw in the last lesson. The second one is the function that you can call to move the promise from pending to rejected. We will call this one reject.
+
+Thus, here's what promises will look like:
+ */
+new Promise((resolve,reject)={
+    resolve(); // will resolve the promise (.then(callback) will be called)
+
+    reject(); // will fail the promise (.catch(callback) will be called)
+})
