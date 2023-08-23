6ba5fd204e00484a486f6f241fc20116dc601ac9
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-22 23:32:23 -0400

    introduction to fetch

--- 42_intro_to_fetch/exo2.js
+++ 42_intro_to_fetch/exo2.js
@@ -1,3 +1,12 @@
 /*
 Complete the checkForNewNotifications function such that it makes a fetch request to https://jsdemo-3f387-default-rtdb.europe-west1.firebasedatabase.app/notifications/new.json, converts the response to JSON format, and logs the data received to the console.
  */
+const url = "https://jsdemo-3f387-default-rtdb.europe-west1.firebasedatabase.app/notifications/ndew.json";
+const checkForNewNotifications = ()=>{
+    return fetch(url).then(response=>response.json()).then(data=>{
+        console.log(data);
+    }).catch(error=>{
+        console.error(error);
+    })
+}
+checkForNewNotifications();
