c03aa01a2b0ff76286d92fa8082407cff38c95ab
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-22 22:14:27 -0400

    json

---
+++ 41_json/json.stringify.js
@@ -0,0 +1,9 @@
+/*
+We use it to convert from an object into a JSON string: JSON.stringify(object) method:
+ */
+const person = {
+    firstName:"john",
+    lastName:"jude",
+    age:90
+};
+console.log(JSON.stringify(person));
