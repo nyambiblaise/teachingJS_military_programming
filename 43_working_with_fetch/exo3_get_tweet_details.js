9ee80d5ae227855072c0d32b0486e712b2834341
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-23 00:07:02 -0400

    working with fetch

---
+++ 43_working_with_fetch/exo3_get_tweet_details.js
@@ -0,0 +1,12 @@
+/*
+Complete the getTweetDetails function such that it fetches the following endpoint https://jsdemo-3f387-default-rtdb.europe-west1.firebasedatabase.app/tweet/1080777336298049537.json and then calls the showAuthorName function with the full name of tweet author (the full name should contain a space character between the first name and the last name).
+ */
+const tweetURL = "https://jsdemo-3f387-default-rtdb.europe-west1.firebasedatabase.app/tweet/1080777336298049537.json";
+
+const getTweetDetails = ()=>{
+
+}
+const showAuthorName = (fullName)=>{
+    console.log(fullName);
+}
+getTweetDetails();
