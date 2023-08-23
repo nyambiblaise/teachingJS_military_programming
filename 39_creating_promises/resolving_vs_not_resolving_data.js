24e4d10d8cb16df369f3152073f23d6d160e8c11
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-22 21:39:48 -0400

    creating promises

--- 39_creating_promises/resolving_vs_not_resolving_data.js
+++ 39_creating_promises/resolving_vs_not_resolving_data.js
@@ -1,5 +1,5 @@
 //NOT RESOLVING DATA
-const time = 1000;
+const time = 4000;
 let uname="john";
 let upass = "mary";
 const loginUser = (username,password)=>{
@@ -11,4 +11,15 @@ const loginUser = (username,password)=>{
 }
 loginUser(uname,upass).then(()=>{
     console.log(`User logged in with username ${uname} and password ${upass}`);
+})
+//RESOLVING THE DATA
+const signInUser = (uname,upass)=>{
+    return new Promise(resolve => {
+        setTimeout(()=>{
+            resolve(uname,upass);
+        },time);
+    })
+}
+signInUser(uname,upass).then((uname,upass)=>{
+    console.log(`Username: ${uname}, Upass: ${upass}`);
 })
