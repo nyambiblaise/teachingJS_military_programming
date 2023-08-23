e0e75abb5f0a5367fa7c603e7a820d44d31ae35e
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-23 00:09:02 -0400

    working with fetch

--- 43_working_with_fetch/exo3_get_tweet_details.js
+++ 43_working_with_fetch/exo3_get_tweet_details.js
@@ -4,7 +4,10 @@ Complete the getTweetDetails function such that it fetches the following endpoin
 const tweetURL = "https://jsdemo-3f387-default-rtdb.europe-west1.firebasedatabase.app/tweet/1080777336298049537.json";
 
 const getTweetDetails = ()=>{
-
+    return fetch(tweetURL).then(response=>response.json()).then(data=>{
+        console.log(data.author.details);
+        showAuthorName(`${data.author.details.firstName} ${data.author.details.lastName}`)
+    })
 }
 const showAuthorName = (fullName)=>{
     console.log(fullName);
