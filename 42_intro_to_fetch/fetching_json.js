e198d36a7fcf0c9f3c352936d3e28a78bede5eef
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-22 22:56:10 -0400

    introduction to fetch

--- 42_intro_to_fetch/fetching_json.js
+++ 42_intro_to_fetch/fetching_json.js
@@ -19,3 +19,9 @@ fetch(URL)
     .then(response => {
         return response.json();
     });
+/*
+This .json() method on response is almost exactly the same as JSON.parse(string). The only difference however is that response.json() is non-blocking and asynchronous meaning that it returns a promise.
+
+What you need to understand is that every time you send a fetch request to a certain API and you expect JSON (which
+ is most of the time), you should convert the response that you get back into JSON format.
+ */
