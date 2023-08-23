0957cf5f11f94474cbe270a798803d7bc35518b5
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-22 22:07:54 -0400

    json

---
+++ 41_json/introduction.js
@@ -0,0 +1,13 @@
+/*
+JSON stands for JavaScript Object Notation. It's a format for storing and sending data. It's very commonly used on the web because it's lightweight. Here's an example of a JSON object in an example file called person.json:
+ */
+{
+    "firstName": "Sam",
+    "lastName": "Green",
+    "age": 24
+}
+const person = {
+    firstName: "Sam",
+    lastName: "Green",
+    age: 24
+};
