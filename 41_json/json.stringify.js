f82f81c61f19fb060cf4041da591062b6a5a99dc
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-22 22:15:08 -0400

    json

--- 41_json/json.stringify.js
+++ 41_json/json.stringify.js
@@ -4,6 +4,7 @@ We use it to convert from an object into a JSON string: JSON.stringify(object) m
 const person = {
     firstName:"john",
     lastName:"jude",
-    age:90
+    age:90,
+    hobbies:["football","hockey","soccer"]
 };
 console.log(JSON.stringify(person));
