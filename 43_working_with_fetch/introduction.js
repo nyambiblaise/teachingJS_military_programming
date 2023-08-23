ff209bc7986c7005309bf5032b26d006a11b5b09
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-22 23:41:46 -0400

    working with fetch

--- 43_working_with_fetch/introduction.js
+++ 43_working_with_fetch/introduction.js
@@ -10,6 +10,7 @@ const todoListURL = "https://jsonplaceholder.typicode.com/todos/1";
 const getToDoListData = ()=>{
     return fetch(todoListURL).then(response=>response.json()).then(data=>{
         console.log(data);
+        return data;
     })
 }
-getToDoListData();
+console.log(getToDoListData());
