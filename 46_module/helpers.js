47c299175e023bc8ff4dd34bf488c419427b339f
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-23 22:37:35 -0400

    modules

---
+++ 46_module/helpers.js
@@ -0,0 +1,5 @@
+class Helper{
+    capitalizeWord(word){
+        return word[0].toUpperCase()+word.substring(1).toLowerCase();
+    }
+}
--- 46_module/introduction.js
+++ 46_module/introduction.js
@@ -1,3 +1,8 @@
 /*
 As the complexity of web application grows, we need to be able to write code in multiple files and reference functions (and variables) from another file. This helps in code reuse, readability, and maintenance.
+
+Every JavaScript file is called a Module or ES Module.
+Every JavaScript file (every module) is separate from the outside world. Every variable you define in a file with let or const is only available in that file, and not in the other files. The same applies to functions & classes.
+
+But, what if we'd like to define a class called Helper in helpers.js:
  */
