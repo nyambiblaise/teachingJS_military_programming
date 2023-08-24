a2a0468d4b034da30dd5dce8e5d8cc9396ed5bfe
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-23 22:48:31 -0400

    modules

---
+++ 46_module/config.js
@@ -0,0 +1,9 @@
+export const serverURL = "localhost";
+export const serverUsername = "root";
+export const serverPort = 8080;
+export const serverSettings = {
+    theme:"dark",
+    logo:"images/logo/logo.png",
+    language:"en",
+    admin_url:"backend"
+}
