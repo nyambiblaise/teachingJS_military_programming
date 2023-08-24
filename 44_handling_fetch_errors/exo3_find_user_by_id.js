da751f1b91cbf96f52393d4f4ae90e664b92540e
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-23 20:28:45 -0400

    handling fetch errors

---
+++ 44_handling_fetch_errors/exo3_find_user_by_id.js
@@ -0,0 +1,15 @@
+const apiURL = "https://jsonplaceholder.typicode.com/users?id=";
+const uID = 10;
+const findUserByID = (userID)=>{
+    return fetch(`${apiURL}${userID}`).then(response=>{
+        if(!response.ok){
+            throw new Error(`Error getting user because ${response.statusText}`)
+        }
+        return response.json();
+    }).then(userData=>{
+        console.log(userData.);
+    }).catch(error=>{
+        console.error(error);
+    })
+}
+findUserByID(uID);
