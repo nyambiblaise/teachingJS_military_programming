b4fbb621973ccf1056d525613daa6b987df2353f
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-23 22:35:18 -0400

    modules

---
+++ 46_module/introduction.js
@@ -0,0 +1,3 @@
+/*
+As the complexity of web application grows, we need to be able to write code in multiple files and reference functions (and variables) from another file. This helps in code reuse, readability, and maintenance.
+ */
