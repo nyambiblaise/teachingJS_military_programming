2198c3ec4e8e2a8cf0e53974ddae2866c6957d7f
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-23 23:12:17 -0400

    modules

---
+++ 46_module/react_example_module.js
@@ -0,0 +1,6 @@
+/*
+If you end up learning a front-end framework/library, you will then have to import the functions/classes provided by that library. Here's an example for React that uses a mix of named imports and default import:
+
+Notice how the React object is the default import and then useState and useContext are named imports.
+ */
+import React, {useState, useContext} from "react";
