202c25f38f646d90fe52a2339ca0a4b0030b9d92
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-23 23:15:29 -0400

    modules

---
+++ 46_module/importing_from_a_library.js
@@ -0,0 +1,6 @@
+/*
+The syntax to import from a library is similar, except that the path should not be a file location (for example, ./) but rather a package name. So, for example:
+
+This needs some special setup for it to work. We will cover that in the future.
+ */
+import _ from "lodash";
