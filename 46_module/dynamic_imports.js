df53aa547fced158d893442afc7fe899af76ff1b
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-23 23:16:37 -0400

    modules

---
+++ 46_module/dynamic_imports.js
@@ -0,0 +1,8 @@
+/*
+Dynamic imports allow you to import another file/module only when necessary. The syntax is similar except that import is used as a function. For example:
+
+You may have noticed that import() (when used as a function) returns a promise. This will be covered in a future chapter.
+ */
+import("./helpers.js").then(module => {
+    console.log(module);
+});
