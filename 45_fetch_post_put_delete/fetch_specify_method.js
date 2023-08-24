07269943e91a208cdbadf678f4e8e81a8f3d477a
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-23 20:37:56 -0400

    fetch post, get, put, delete methods

---
+++ 45_fetch_post_put_delete/fetch_specify_method.js
@@ -0,0 +1,10 @@
+/*
+We talked about the fetch() method accepting the URL as a first argument. However, the fetch() method also accepts a 2nd argument that is optional.
+
+In this second option, you can specify:
+
+the method of the request (GET/POST/PUT/DELETE)
+the headers you'd like to send
+the body you'd like to send
+Let's start with the method. The method defaults to GET. So, with the following code:
+ */
