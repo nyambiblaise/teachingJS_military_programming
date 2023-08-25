268d3d3be853be023d7c2d51236e4842dd78b230
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-24 22:19:45 -0400

    fetch wrapper

--- 47_fetch_wrapper/implementing_GET_with_FetchWrapper_class.js
+++ 47_fetch_wrapper/implementing_GET_with_FetchWrapper_class.js
@@ -1,15 +1,11 @@
 class FetchWrapper{
     constructor(apiUrl) {
         this.apiUrl = apiUrl;
-        fetch(url).then(response=>{
-            if(!response.ok){
-                throw new Error(`Something went wrong with API`);
-            }
-            return response=>response.json();
-        })
+
     }
     get(endPoint){
-
+        return fetch(url + endPoint)
+                .then(response=>response.json());
     }
 }
 const getData = new FetchWrapper("");
@@ -27,3 +23,11 @@ fetch(url).then(response=>{
 }).catch(error=>{
     return error;
 })
+
+/*
+Important things to note:
+
+The return is very important. It is what allows us to call .then(data => ...) on API.get(...). That's because it returns the result of fetch().then().
+Don't forget to prefix the endpoint with the this.baseURL.
+Since we know that the API is always going to return JSON, then we convert the response to JSON inside the get() method so that we don't have to do that outside the FetchWrapper class.
+ */
