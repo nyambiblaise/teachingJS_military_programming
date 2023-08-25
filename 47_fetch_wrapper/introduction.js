301f6379861446aeba6e16e280b75cdb419320b2
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-24 21:54:17 -0400

    fetch wrapper

---
+++ 47_fetch_wrapper/introduction.js
@@ -0,0 +1,8 @@
+/*
+A fetch wrapper is not absolutely necessary, but, it does make working with fetch easier and more eloquent.
+
+A fetch wrapper is a class that wraps the fetch API in a way that works specifically for our scenario. When building a website as web developers, you and I will often work with the following:
+
+mostly the same API (same base URL but different endpoints)
+a JSON API (an API that returns JSON)
+ */
