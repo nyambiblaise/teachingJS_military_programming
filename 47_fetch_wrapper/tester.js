51bb37e456bc40a44f7d9517e0e5b242cb6922d9
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-24 22:43:53 -0400

    fetch wrapper

--- 47_fetch_wrapper/tester.js
+++ 47_fetch_wrapper/tester.js
@@ -1,6 +1,6 @@
 import FetchWrapper from "./FetchWrapper.js";
 
-const serverURL = "https://jsdemo-3f387-default-rtdb.europe-west1.firebasedatabase.app/notifications/new.json";
+const serverURL = "https://jsdemo-3f387-default-rtdb.europe-west1.firebasedatabase.app";
 const fetchWrap = new FetchWrapper(serverURL);
 
 fetchWrap.get("/notifications/new.json").then(data=>{
