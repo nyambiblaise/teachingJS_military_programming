26bdfadd2f2339d67ee43a5166e85b9baa96a9a3
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-25 21:49:32 -0400

    fetch wrapper

--- 47_fetch_wrapper/FetchWrapper.js
+++ 47_fetch_wrapper/FetchWrapper.js
@@ -20,4 +20,25 @@ export default class FetchWrapper{
         })
             .then(response=>response.json());
     }
+    put2(endpoint, body) {
+        return this.#send("put", endpoint, body);
+    }
+
+    post(endpoint, body) {
+        return this.#send("post", endpoint, body);
+    }
+
+    delete(endpoint, body) {
+        return this.#send("delete", endpoint, body);
+    }
+
+    #send(method, endpoint, body) {
+        return fetch(this.baseURL + endpoint, {
+            method,
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(body)
+        }).then(response => response.json());
+    }
 }
