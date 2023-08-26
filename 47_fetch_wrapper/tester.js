5512578259b57897f4c7376639307a92dd5ba70b
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-25 21:19:31 -0400

    fetch wrapper

--- 47_fetch_wrapper/tester.js
+++ 47_fetch_wrapper/tester.js
@@ -1,8 +1,16 @@
 import FetchWrapper from "./FetchWrapper.js";
 
 const serverURL = "https://jsdemo-3f387-default-rtdb.europe-west1.firebasedatabase.app";
-const fetchWrap = new FetchWrapper(serverURL);
+//const fetchWrap = new FetchWrapper(serverURL);
 
-fetchWrap.get("/notifications/new.json").then(data=>{
+/*fetchWrap.get("/notifications/new.json").then(data=>{
+    console.log(data);
+})*/
+const postURL = "https://api.learnjavascript.online/";
+const data = {
+    grade:16
+}
+const fetchWrap2 = new FetchWrapper(postURL);
+fetchWrap2.put("demo/grades.json",data).then(data=>{
     console.log(data);
 })
