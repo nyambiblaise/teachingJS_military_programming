9d42e23ccd3909b9a22800264e841017244823b3
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-25 21:47:48 -0400

    fetch wrapper

--- 47_fetch_wrapper/implementing_POST_and_DELETE_with_FetchWrapper_class.js
+++ 47_fetch_wrapper/implementing_POST_and_DELETE_with_FetchWrapper_class.js
@@ -18,3 +18,34 @@ class FetchWrapper {
 The #send() method takes the method ("put", "post", or "delete") followed by the endpoint and the body.
 So, now, we can implement the put(), post(), and delete() methods that call this internal private #send() method:
  */
+class FetchWrapper {
+    // constructor() and get()
+
+    put(endpoint, body) {
+        // pass the endpoint and body parameters to #send
+        // and specify the method to be 'put'
+        return this.#send("put", endpoint, body);
+    }
+
+    post(endpoint, body) {
+        // pass the endpoint and body parameters to #send
+        // and specify the method to be 'post'
+        return this.#send("post", endpoint, body);
+    }
+
+    delete(endpoint, body) {
+        // pass the endpoint and body parameters to #send
+        // and specify the method to be 'delete'
+        return this.#send("delete", endpoint, body);
+    }
+
+    #send(method, endpoint, body) {
+        return fetch(this.baseURL + endpoint, {
+            method, // object shorthand
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(body)
+        }).then(response => response.json());
+    }
+}
