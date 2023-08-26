249ced88a27ff2d183e6f431eefcf1ca3fefb732
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-25 21:46:51 -0400

    fetch wrapper

--- 47_fetch_wrapper/implementing_POST_and_DELETE_with_FetchWrapper_class.js
+++ 47_fetch_wrapper/implementing_POST_and_DELETE_with_FetchWrapper_class.js
@@ -1,3 +1,16 @@
 /*
 the post() and delete() methods are very similar to the put(). The only thing that changes is the method: "put".
  */
+class FetchWrapper {
+    // constructor() and get()
+
+    #send(method, endpoint, body) {
+        return fetch(this.baseURL + endpoint, {
+            method: method, // this is now dynamic
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(body)
+        }).then(response => response.json());
+    }
+}
