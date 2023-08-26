c25e71c0fb2ffdd4dbeab0cdfaaa911cda19193c
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-25 21:22:40 -0400

    fetch wrapper

--- 47_fetch_wrapper/implementing_POST_with_FetchWrapper_class.js
+++ 47_fetch_wrapper/implementing_POST_with_FetchWrapper_class.js
@@ -1,3 +1,11 @@
 /*
+we'll implement the put(endpoint, dataToPost) method of the FetchWrapper class.
 
+The put(endpoint, dataToPost) method is doing a lot of work behind the scenes:
+
+it's sending a PUT request to https://api.xyz.com/demo/grades.json (baseURL followed by endpoint)
+it's sending a custom header "Content-Type": "application/json"
+it's converting the body argument to a JSON string (with JSON.stringify) and sending it as the body of the fetch request
+it's converting the server response to json (response => response.json())
+From that, we can deduce that the put(endpoint, body) implementation looks like the following:
  */
