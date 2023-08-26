c2138ea09a41d4666b20587efbad25935cd39b5c
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-25 21:34:41 -0400

    fetch wrapper

--- 47_fetch_wrapper/5_put_user_grades.js
+++ 47_fetch_wrapper/5_put_user_grades.js
@@ -8,9 +8,16 @@ Base URL: https://api.learnjavascript.online/demo/ Endpoint: grades.json Method:
 Body: {grade: 15} (replace 15 with the actual grade)
 
 Please note that fetch requests to these demo APIs are throttled. If you request it too many times in a short amount of time, you will have to wait 60 seconds until you're able to access it again. This is to prevent (accidental) abuse.
-
-We recommend that you use the exported FetchWrapper from fetch-wrapper.js.
  */
 import FetchWrapper from "./FetchWrapper.js";
 const url = "https://api.learnjavascript.online/demo/";
 const fetchWrapper = new FetchWrapper(url);
+const data = {
+    grade:17
+}
+const saveGrade = (grade)=>{
+    fetchWrapper.put("grades.json",grade).then(d=>{
+        console.log(d)
+    })
+}
+saveGrade(data)
