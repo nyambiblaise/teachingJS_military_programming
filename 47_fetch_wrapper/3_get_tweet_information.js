b7c3ae4842d2e8ed5f85a168cebd2555e968a2a1
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-29 23:36:38 -0400

    DOM Selection 1

--- 47_fetch_wrapper/3_get_tweet_information.js
+++ 47_fetch_wrapper/3_get_tweet_information.js
@@ -1,6 +1,5 @@
 /*
 Complete the getTweetDetails function such that it fetches the following endpoint https://jsdemo-3f387-default-rtdb.europe-west1.firebasedatabase.app/tweet/1080777336298049537.json and then calls the showAuthorName function with the full name of tweet author (the full name should contain a space character between the first name and the last name).
-
 We recommend that you use the exported FetchWrapper from fetch-wrapper.js.
  */
 import FetchWrapper from "./FetchWrapper.js";
