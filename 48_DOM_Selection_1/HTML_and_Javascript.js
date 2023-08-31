a02598db07b4bc9c6f07d448b4c111fcb91628ef
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-30 22:09:58 -0400

    DOM Selection 1

--- 48_DOM_Selection_1/HTML_and_Javascript.js
+++ 48_DOM_Selection_1/HTML_and_Javascript.js
@@ -1,3 +1,3 @@
 /*
-f
+The DOM (Document Object Model) is a JavaScript object & API that represents the HTML of your page. It lets you interact from JavaScript with the elements on your page. You can read and change text, add and delete items, and a lot more.
  */
