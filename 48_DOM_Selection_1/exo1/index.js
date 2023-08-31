19603a5552eea771b3da33d48815b57a4d59e8f7
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-30 22:52:38 -0400

    DOM Selection 1

--- 48_DOM_Selection_1/exo1/index.js
+++ 48_DOM_Selection_1/exo1/index.js
@@ -2,3 +2,7 @@
 Complete the method getFooterElement such that it finds and returns the element with the id footer-wrapper from the HTML page.
 Make sure to take a look at the code in the index.html file.
  */
+const getFooterElement = ()=>{
+    return document.querySelector("footer-wrapper");
+}
+console.log(getFooterElement());
