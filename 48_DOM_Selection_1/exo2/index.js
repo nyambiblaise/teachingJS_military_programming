24420fb1f5736cbd5a64ac24027ecd995f8825cd
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-30 22:57:54 -0400

    DOM Selection 1

---
+++ 48_DOM_Selection_1/exo2/index.js
@@ -0,0 +1,7 @@
+/*
+Complete the 4 functions such that they return what is expected. Every method has a comment explaining what it needs to return.
+getNavbarElement(): get the element with id navbar
+getMainElement(): get the element with id main
+getAboutFromFooter() : get the about link that's in the footer
+getTheParagraphElement() : get the first paragraph element
+ */
