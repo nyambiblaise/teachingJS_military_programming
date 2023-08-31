a5d15c25cff8bbdb910685c64115368952203560
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-30 22:49:32 -0400

    DOM Selection 1

---
+++ 48_DOM_Selection_1/exo1/index.js
@@ -0,0 +1,4 @@
+/*
+Complete the method getFooterElement such that it finds and returns the element with the id footer-wrapper from the HTML page.
+Make sure to take a look at the code in the index.html file.
+ */
