f0323df0a4e6b594115aae5f23d0cbcc1a094a81
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-31 20:32:39 -0400

    DOM Selection 1

--- 48_DOM_Selection_1/exo3/index.js
+++ 48_DOM_Selection_1/exo3/index.js
@@ -1,3 +1,9 @@
 /*
 Complete the getArticleHeadline function such that it returns the headline of the article.
+
  */
+const getArticleHeadline = ()=>{
+    const head = document.querySelector(".container #headline").textContent;
+    return head;
+}
+console.log(getArticleHeadline());
