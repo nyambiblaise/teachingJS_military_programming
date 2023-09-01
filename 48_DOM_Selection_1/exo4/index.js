703c90aeb79355b2ec45acbafd6e33475d6ff82c
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-08-31 21:10:25 -0400

    DOM Selection 1

---
+++ 48_DOM_Selection_1/exo4/index.js
@@ -0,0 +1,7 @@
+/*
+The user is asked to select a currency by clicking on it. When a .card is clicked, we add the class active to it and remove the class active from the previous card. So, only one currency can be selected at a time. Try it out in the browser tab by clicking on the currencies!
+
+This highlighted effect is coming from the class active that you can find at the end of the index.css.
+
+Complete the getSelectedCurrency function such that it returns the currency that the user selected. Return the text from the card that is selected.
+ */
