dca2b9dbafddd6460357502c49cdd766e23eef63
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-09-01 22:58:46 -0400

    DOM Selection 2

---
+++ 49_DOM_Selection_2/exo4/index.js
@@ -0,0 +1,6 @@
+//Complete the logLinksTexts function such that it logs the text of every link to the console.
+const logLinksTexts = ()=>{
+    document.querySelectorAll("a").forEach(link=>{
+        console.log(link.textContent);
+    })
+}
