ea54123f2cd24ce3e56b97174a0c7e086db389fb
Nyambi Blaise <34681563+nyambiblaise@users.noreply.github.com>
2026-01-02 16:59:59 -0500

    Update introduction.js

--- 18_nullish_coalescing/introduction.js
+++ 18_nullish_coalescing/introduction.js
@@ -4,7 +4,6 @@ The nullish coalescing ?? operator is a new operator introduced in JavaScript th
 A nullish value is a value that is either null or undefined.
 
 Notice how when name is a nullish value (either null or undefined), then the right-hand side of the operator is executed. In this case, "N/A".
-
 This operator is useful to avoid showing undefined or null to the User Interface, which are often signs of bugs.
 Whenever possible, if you can provide a default value, you can use the nullish coalescing operator ?? to show that default value.
  */
